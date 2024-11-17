02c73b8bbb6acfaccf83ea1d94a2d1a8e7c720a4
111Satish <satishjnvr@gmail.com>
2024-11-17 22:25:51 +0530

    fixed

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI); 
+    const conn = await mongoose.connect('mongodb+srv://satishjnvr:qEyy9BR9_xjc3VQ@cluster0.e7sno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- src/config/razorpayConfig.js
+++ src/config/razorpayConfig.js
@@ -2,8 +2,8 @@ const Razorpay = require('razorpay');
 require('dotenv').config();
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: 'rzp_test_d9SMQlvCWU5QsL',
+  key_secret: 'SPZ5O9ES8Uw25efTCSQMbGQ8',
 });
 
 module.exports = razorpay;
